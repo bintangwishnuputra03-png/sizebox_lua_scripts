40622690a7e7175e98127dfc7f5ebbd863308da2
bintangwishnuputra03-png <bintang.wishnuputra03@gmail.com>
2025-11-05 21:31:51 +0800

    Update Handhelds_d.lua with new descriptions
    Added descriptions for Handhelds and their animations.

--- Handhelds_d.lua
+++ Handhelds_d.lua
@@ -1,2 +1,5 @@
-Handhelds_d.lua
-Hands together_d.Lua
+gts.Handhelds_d.lua
+Handhelds join giantess girls on players. Together just like of you.
+Animation custom Handhelds together. Yes we can
+Show you here.
+Catch the micros.
